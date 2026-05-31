f27a52bfc6bbfd5ed26dcd963331ff99321c90bc
Rakshit-Rangarajan <rakshitr2000@gmail.com>
2026-06-01 03:01:15 +0530

    build: untrack node_modules and vite cache, and restore standard frontend build scripts

--- frontend/.vite-cache/deps/react-dom.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-C4GXA2H5.js";
-import "./chunk-MWU27ZKJ.js";
-export default require_react_dom();
--- frontend/.vite-cache/deps/react.js
+++
@@ -1,4 +0,0 @@
-import {
-  require_react
-} from "./chunk-MWU27ZKJ.js";
-export default require_react();
